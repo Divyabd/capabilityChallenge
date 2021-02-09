1c3f7dc5e8bc67678e3728caa943feee6386ba2c
Divyabd <divyagowda8393@gmail.com>
2021-02-09 09:22:25 +0530

    master commit

---
+++ HelloWord/src/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+		System.out.println(a+b);
+		System.out.println("mindtree.com");
+	}
+
+}
